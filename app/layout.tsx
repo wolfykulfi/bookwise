00e59bd96bb8ce3c8b1928b61abf01830ab99d69
Utkarsh <141806996+wolfykulfi@users.noreply.github.com>
2025-01-14 17:41:47 +0530

    ini

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+import localFont from "next/font/local";
+import { ReactNode } from "react";
+
+const ibmPlexSans = localFont({
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+});
+
+const bebasNeue = localFont({
+  src: [
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: "--bebas-neue",
+});
+
+export const metadata: Metadata = {
+  title: "BookWise",
+  description:
+    "BookWise is a book borrowing university library management solution.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+          className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+          >
+        {children}
+      </body>
+    </html>
+  );
+}
